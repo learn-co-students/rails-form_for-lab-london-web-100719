085d12d34ce041083d9f4ca06f854c774f775412
gawaxx <jules.blanc@unccub.com>
2019-11-03 23:22:43 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,31 +1,28 @@
 class StudentsController < ApplicationController
-    def show
-      @student = Student.find(params[:id])
+
+    def show 
+        @student = Student.find(params[:id])
     end
-  
-    def new
-      @student = Student.new
+
+    def new 
+        @student = Student.new
     end
-  
-    def edit
-      @student = Student.find(params[:id])
+
+    def edit 
+        @student = Student.find(params[:id])
     end
-  
-    def create
-      @student = Student.new(student_params)
-      @student.save
-      redirect_to student_path(@student)
+
+    def create 
+        @student = Student.new
+        @student.first_name = params[:student][:first_name]
+        @student.last_name = params[:student][:last_name]
+        @student.save
+        redirect_to student_path(@student)
     end
-  
-    def update
-      @student = Student.find(params[:id])
-      @student.update(student_params)
-      redirect_to student_path(@student)
+
+    def update 
+        @student = Student.find(params[:id])
+        @student.update(params.require(:student).permit(:first_name, :last_name))
+        redirect_to student_path(@student)
     end
-  
-    private
-  
-    def student_params
-      params.require(:student).permit!
-    end
-  end
+end 
