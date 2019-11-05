bebcf52d52ab7aa54af441c20583936c65fe6292
Ned-el-ch <nedelchev.niki@gmail.com>
2019-11-05 13:01:42 +0000

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,78 @@
+class SchoolClassesController < ApplicationController
+    def show
+      @school_class = SchoolClass.find(params[:id])
+    end
+  
+    def new
+      @school_class = SchoolClass.new
+    end
+  
+    def edit
+      @school_class = SchoolClass.find(params[:id])
+    end
+  
+    def create
+      @school_class = SchoolClass.new(school_class_params)
+      @school_class.save
+      redirect_to school_class_path(@school_class)
+    end
+  
+    def update
+      @school_class = SchoolClass.find(params[:id])
+      @school_class.update(school_class_params)
+      redirect_to school_class_path(@school_class)
+    end
+  
+    private
+  
+    def school_class_params
+      params.require(:school_class).permit!
+    end
+  end
+
+# class SchoolClassesController < ApplicationController
+
+#     # before_action :school_class
+    
+#     def index
+#         @school_classes = SchoolClass.all
+#     end
+
+#     def show
+#         school_class
+#     end
+
+#     def new
+#         @school_class = SchoolClass.new
+#     end
+
+#     def create
+#         @school_class = SchoolClass.create(school_class_params)
+#         redirect_to school_class_path(@school_class)
+#     end
+
+#     def edit
+#         school_class
+#     end
+
+#     def update
+#         school_class
+#         redirect_to school_class_path(@school_class)
+#     end
+
+#     # def destroy
+#     #     school_class
+#     # end
+
+#     private
+
+#     def school_class
+#         @school_class = SchoolClass.find(params[:id])
+#     end
+
+#     def school_class_params
+#         # byebug
+#         params.require(:school_class).permit(:title, :room_number)
+#     end
+
+# end
